cc878ccab9dbc3c7c75645cd0bc7a2feb75e41aa
Alorel <a.molcanovas@gmail.com>
2015-09-20 22:50:13 +0100

    PHPUnit autoloader removed

--- phpunit.php
+++ phpunit.php
@@ -5,23 +5,3 @@
     foreach ($composerJSON['autoload']['files'] as $f) {
         include_once $f;
     }
-
-    function log__aloframework__phpunitAutoloader($class) {
-        $class = ltrim($class, '\\');
-        $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-        $file  =
-            __DIR__ .
-            DIRECTORY_SEPARATOR .
-            'src' .
-            DIRECTORY_SEPARATOR .
-            'class' .
-            DIRECTORY_SEPARATOR .
-            $class .
-            '.php';
-
-        if (file_exists($file)) {
-            include_once $file;
-        }
-    }
-
-    spl_autoload_register('log__aloframework__phpunitAutoloader');
