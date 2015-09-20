383ec2409c72864066b76f534f8947eb38f94855
Alorel <a.molcanovas@gmail.com>
2015-09-20 21:33:09 +0100

    removed some bloat from docs

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","AloFramework\\Log\\Log"],["c","AloFramework\\Log\\LogException"],["c","Exception"],["c","InvalidArgumentException"],["c","LogicException"],["c","Psr\\Log\\AbstractLogger"],["c","Psr\\Log\\InvalidArgumentException"],["c","Psr\\Log\\LoggerAwareInterface"],["c","Psr\\Log\\LoggerAwareTrait"],["c","Psr\\Log\\LoggerInterface"],["c","Psr\\Log\\LoggerTrait"],["c","Psr\\Log\\LogLevel"],["c","Psr\\Log\\NullLogger"],["c","Psr\\Log\\Test\\DummyTest"],["c","Psr\\Log\\Test\\LoggerInterfaceTest"]];
+ApiGen.elements = [["c","AloFramework\\Log\\Log"],["c","AloFramework\\Log\\LogException"],["c","Exception"]];
