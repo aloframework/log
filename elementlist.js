146f010cea0c34b560f2b47d2e47ccc510e2b628
Alorel <a.molcanovas@gmail.com>
2015-10-14 19:59:08 +0100

    changelog

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","AloFramework\\Log\\Config"],["c","AloFramework\\Log\\InvalidArgumentException"],["c","AloFramework\\Log\\Log"],["c","Exception"],["c","InvalidArgumentException"],["c","LogicException"]];
